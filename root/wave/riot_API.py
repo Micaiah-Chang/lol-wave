b55f812db474221eeea984945ec4527684d093ba
Micaiah-Chang <Micaiah@server.fake>
2014-08-01 17:54:05 -0700

    Fix bug where riot_API was not correctly sending summoner ID info.
    In addition, also refactor to appending API key to URL.

--- root/wave/riot_API.py
+++ root/wave/riot_API.py
@@ -2,17 +2,17 @@ import json
 import urllib, urllib2
 import os
 
-def run_query(summoner):
+# Warning! Currently region is only set to NA!
+
+BLUE_ID = 100
+PURPLE_ID = 200
+
+
+def get_id(summoner):
     root_url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'
 
-    curr_dir = os.path.dirname(__file__)
-    curr_dir = os.path.abspath(curr_dir)
-    API_file = os.path.join(curr_dir, "API.txt")
-
-    with open(API_file, 'r') as f:
-        API_key = f.readline().strip()
-
-    url= "{0}{1}?api_key={2}".format(root_url, summoner, API_key)
+    summon_url = "{0}{1}".format(root_url, summoner)
+    url = append_API_key(summon_url)
     results = {}
 
 
@@ -20,31 +20,21 @@ def run_query(summoner):
         response = urllib2.urlopen(url).read()
         json_response = json.loads(response)
 
-        results['name'] = json_response[summoner]['name']
+        results['id'] = json_response[summoner][u'id']
     except urllib2.URLError, e:
         print "Error when querying Riot games:", e
         print "Best Technical"
 
-
-    return results
-
+    return results['id']
+# Results is a bad name. Refactor to something else.
 
 def get_version():
     API_ver = "1.2"
 
     root_url = "https://na.api.pvp.net/api/lol/static-data/na/v{0}/versions".format(API_ver)
 
-    curr_dir = os.path.dirname(__file__)
-    curr_dir = os.path.abspath(curr_dir)
-    API_file = os.path.join(curr_dir, "API.txt")
-
-    with open(API_file, 'r') as f:
-        API_key = f.readline()
-
-    url = "{0}?api_key={1}".format(root_url ,API_key)
-
     version = None
-
+    url = append_API_key(root_url)
     try:
         response = urllib2.urlopen(url).read()
         json_response = json.loads(response)
@@ -55,9 +45,18 @@ def get_version():
 
     return version
 
+def append_API_key(url):
+    curr_dir = os.path.dirname(__file__)
+    curr_dir = os.path.abspath(curr_dir)
+    API_file = os.path.join(curr_dir, "API.txt")
+
+    with open(API_file, 'r') as f:
+        API_key = f.readline()
+
+    result_url = "{0}?api_key={1}".format(url ,API_key)
+    return result_url
+
 
 if __name__ == '__main__':
-    results = run_query("meisnewbie")
-    print results
-    version = get_version()
-    print version
+    s_id = get_id("meisnewbie")
+    latest_game(s_id)
--- root/wave/views.py
+++ root/wave/views.py
@@ -1,11 +1,17 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from wave.riot_API import get_version
+from wave.riot_API import get_version, get_id
 
 def index(request):
     context = RequestContext(request)
 
     version = get_version()
     context_dict = {'version': version}
+    if request.method == 'POST':
+        summoner = request.POST['query'].strip()
+        result = get_id(summoner)
+        context_dict['result_list'] = result
+
+
     return render_to_response("wave/index.html", context_dict, context)
