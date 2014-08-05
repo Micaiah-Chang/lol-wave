017ddceaa28269cd7c29cb18a114ad993c8d9973
Micaiah-Chang <Micaiah@server.fake>
2014-08-04 21:14:50 -0700

    Update index to print out summoner ID for person who is typed in.

--- root/wave/views.py
+++ root/wave/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 
 from wave.riot_API import get_version, get_id
 
+
 def index(request):
     context = RequestContext(request)
 
@@ -11,7 +12,7 @@ def index(request):
     if request.method == 'POST':
         summoner = request.POST['query'].strip()
         result = get_id(summoner)
-        context_dict['result_list'] = result
+        context_dict['id'] = result
 
 
     return render_to_response("wave/index.html", context_dict, context)
