e905bcf5ba0bb8c800ec6902174cccfe416574f3
Micaiah-Chang <Micaiah@server.fake>
2014-08-05 21:54:38 -0700

    Update search resutls to return curated player stats for most recent game.
    Index page returns wards killed/placed as well as damage dealt/taken.
    This is shown in an unordered bullet point list. Currently the
    search results do not show what champions were played, nor what
    teams they are on. Things are starting to look up!

--- root/wave/views.py
+++ root/wave/views.py
@@ -1,7 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from wave.riot_API import get_version, get_id
+from wave.riot_API import get_version, get_team_data
 
 
 def index(request):
@@ -10,9 +10,12 @@ def index(request):
     version = get_version()
     context_dict = {'version': version}
     if request.method == 'POST':
-        summoner = request.POST['query'].strip()
-        result = get_id(summoner)
-        context_dict['id'] = result
+        summoner_name = request.POST['query'].strip()
+        results = get_team_data(summoner_name)
+        name, stats = results.pop(0)
+        team = [(t_name, zip(t_stats.keys(), t_stats.values())) for t_name, t_stats in results]
+        context_dict['name'] = name
+        context_dict['teammates'] = team
 
 
     return render_to_response("wave/index.html", context_dict, context)
